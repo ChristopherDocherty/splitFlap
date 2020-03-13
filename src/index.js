0a919b8309a3a45c03a0e8bdb035464ed11de89b
pd-9999 <51860817+pd-9999@users.noreply.github.com>
2020-03-13 23:08:23 +0000

    Beginning to understand react

--- src/index.js
+++ src/index.js
@@ -1,12 +1,90 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Panel extends React.Component{
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
+    constructor(props){
+        super(props);
+        
+
+        this.state = {
+            topLetter: "C",
+            bottomLetter: "D",
+            alphabet: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+            count: 0,
+        }
+    }
+
+
+    componentDidMount() {
+        //Need to update css to add animation
+
+        console.log("test");
+        this.timerID = setInterval(
+            () => this.next_letters(),
+            1000
+          );
+
+    }
+
+
+    next_letters() {
+
+        //call animation
+        
+
+        
+        //Update letter
+        this.setState({count: this.state.count +1});
+        this.setState({topLetter: this.state.alphabet[this.state.count]});
+        console.log(this.state.alphabet[this.state.count]);
+    }
+
+    componentWillUnmount() {
+
+    }
+
+
+    render(){
+        return (
+            <div className="flip-container">
+
+            <div className="top">
+                <div className="letter-top"> {this.state.bottomLetter} </div>
+            </div>
+        
+            <div className="flipper">
+        
+        		<div className="front">
+        			<div className="letter-top"> {this.state.topLetter} </div>
+        		</div>
+
+        		<div className="back">
+        			<div className="letter-bottom"> {this.state.bottomLetter} </div>
+        		</div>
+        
+        	</div>
+        
+            <div className="bottom">
+                <div className="letter-bottom"> {this.state.topLetter} </div>
+            </div>
+        </div>
+        );
+    }
+}
+
+
+
+
+
+
+
+
+
+ReactDOM.render(
+    <Panel />,
+    document.getElementById('root')
+);
